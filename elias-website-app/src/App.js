1f2509730271ab7156eacb1ff2e8ca3342141c0e
eliasmeana132 <eliasmeana132@gmail.com>
2024-11-17 00:04:42 -0500

    Adding some pages

--- elias-website-app/src/App.js
+++ elias-website-app/src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import './App.css';
-import Sidebar from './components/Sidebar';
-import Content from './components/Content';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SidebarLayout from './layouts/SidebarLayout';
+import NoSidebarLayout from './layouts/NoSidebarLayout';
+import Home from './components/Home';
+import Resume from './components/Resume';
+import './styles/App.css';
+import Navbar from './components/Navbar';
+import Birla from './components/Birla';
 
 function App() {
   return (
-    <div className="container">
-      <Sidebar />
-      <Content />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        {/* Route with Sidebar */}
+        <Route path="/" element={<NoSidebarLayout><Home /></NoSidebarLayout>} />
+        <Route path="/resume" element={<SidebarLayout><Resume /></SidebarLayout>} />
+        <Route path="/birla" element={<NoSidebarLayout><Birla /></NoSidebarLayout>} />
+        {/* Routes without Sidebar
+        <Route path="/about" element={<NoSidebarLayout><About /></NoSidebarLayout>} />
+        <Route path="/contact" element={<NoSidebarLayout><Contact /></NoSidebarLayout>} /> */}
+      </Routes>
+    </Router>
   );
 }
 

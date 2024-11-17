0b752d7b3b272e57899508007429136c23858fb3
eliasmeana132 <57874712+eliasmeana132@users.noreply.github.com>
2024-11-17 17:18:34 -0500

    Update App.js

--- elias-website-app/src/App.js
+++ elias-website-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//yimport { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import SidebarLayout from './layouts/SidebarLayout';
 import NoSidebarLayout from './layouts/NoSidebarLayout';

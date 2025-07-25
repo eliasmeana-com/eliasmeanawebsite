1c6b2bad6f14f4f590217858c92d106448afa950
eliasmeana132 <57874712+eliasmeana132@users.noreply.github.com>
2025-07-25 16:14:40 -0400

    correcting assignment schema

--- elias-website-app/backend/models/Assignments.js
+++ elias-website-app/backend/models/Assignments.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const latexCodeSchema = new mongoose.Schema({
+const assignmentSchema = new mongoose.Schema({
   latexCode : String,
   pageCode : String,
   assignmentName : String,
   dueDate : String
 }, { collection: 'CLASS_ASSIGNMENTS' }); 
-module.exports = mongoose.model('LatexCode', latexCodeSchema);
+module.exports = mongoose.model('LatexCode', assignmentSchema);

38fb5feac1eb95f273b4d67bbd397b485d01c6e4
eliasmeana132 <eliasmeana132@gmail.com>
2025-07-24 21:57:01 -0400

    adding backend service for mongodb

---
+++ elias-website-app/backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const connectDB = require('./config/db');
+const scheduleRoutes = require('./routes/scheduleRoutes.js');
+
+const app = express();
+const PORT = process.env.PORT || 6000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Routes
+app.use('/api/schedule', scheduleRoutes);
+
+// Test Route
+app.get('/test-connection', (req, res) => {
+  res.json({ status: 'MongoDB Connected' });
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

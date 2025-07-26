4a610d1b82778f41ce1e1b6c6e1ff3b73aad005d
eliasmeana132 <eliasmeana132@gmail.com>
2025-07-26 10:32:49 -0400

    removing backend repo

--- elias-website-app/backend/models/Assignments.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const assignmentSchema = new mongoose.Schema({
-  latexCode : String,
-  classCode : String,
-  assignmentName : String,
-  dueDate : String
-}, { collection: 'CLASS_ASSIGNMENTS' }); 
-module.exports = mongoose.model('assignment', assignmentSchema);
--- elias-website-app/backend/models/ClassNotesLatex.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const classNoteSchema = new mongoose.Schema({
-  latexCode : String,
-  pageCode : String
-}, { collection: 'CLASS_NOTES' }); 
-module.exports = mongoose.model('LatexCode', classNoteSchema);
